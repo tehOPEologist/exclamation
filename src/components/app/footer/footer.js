8b2405c001ea83597a5ef8d4cfb7453b50d22cbc
Aaron Giordano-Barry <tehopeologist@gmail.com>
2017-03-26 22:11:03 -0700

    added links to footer

--- src/components/app/footer/footer.js
+++ src/components/app/footer/footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './footer.css';
+import Links from './links';
 
 export default () => <footer className={ styles.footer }>
-    Woot Logos, Site Design, & Content &copy; Woot, Inc. 2004-{ new Date().getFullYear() }. All Rights Reserved.
+    <a href="" className={ styles.customerService }>Customer Service</a>
+    <Links />
+    <p className={ styles.copyright }>Woot Logos, Site Design, & Content &copy; Woot, Inc. 2004-{ new Date().getFullYear() }. All Rights Reserved.</p>
 </footer>;
